c8fabfb2c6bc0f87073eeebd771f31eb22d0f74c
Aimdi <118352731+Aimdi@users.noreply.github.com>
2026-09-25 00:38:37 +0200

    feat(home): modernize desktop source workflow

--- lib/home/_feed.dart
+++ lib/home/_feed.dart
@@ -2,17 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:pref/pref.dart';
 import 'package:provider/provider.dart';
 import 'package:qui/constants.dart';
-import 'package:qui/home/_for_you.dart';
-import 'package:qui/tweet/paginated_tweet_list.dart';
 import 'package:qui/generated/l10n.dart';
 import 'package:qui/group/_feed_shell.dart';
 import 'package:qui/group/group_model.dart';
 import 'package:qui/group/group_screen.dart';
+import 'package:qui/home/_for_you.dart';
+import 'package:qui/home/home_timeline_controls.dart';
 import 'package:qui/plugins/reddit/reddit_feed_list.dart';
+import 'package:qui/tweet/paginated_tweet_list.dart';
 
 typedef FeedTabTitleBuilder = String Function(BuildContext context);
 
 enum FeedTab { following, foryou, reddit }
+
+IconData feedTabIcon(FeedTab tab) => switch (tab) {
+  FeedTab.following => Icons.home_outlined,
+  FeedTab.foryou => Icons.auto_awesome_outlined,
+  FeedTab.reddit => Icons.reddit_outlined,
+};
 
 class FeedTabOption {
   final FeedTab id;
@@ -33,52 +40,83 @@ final List<FeedTabOption> feedTabs = [
 /// empty screen would be worse than no entry, and the choice is stored by name
 /// so turning the plugin off simply stops offering it.
 List<FeedTabOption> availableFeedTabs(BasePrefService prefs) => feedTabs
-    .where((e) => e.id != FeedTab.reddit || prefs.get<bool>(optionPluginRedditEnabled) == true)
+    .where(
+      (e) =>
+          e.id != FeedTab.reddit ||
+          prefs.get<bool>(optionPluginRedditEnabled) == true,
+    )
     .toList(growable: false);
 
-FeedTab feedTabFromId(String? id) =>
-    FeedTab.values.firstWhere((e) => e.name == id, orElse: () => FeedTab.following);
+FeedTab feedTabFromId(String? id) => FeedTab.values.firstWhere(
+      (e) => e.name == id,
+      orElse: () => FeedTab.following,
+    );
 
 class FeedScreen extends StatefulWidget {
   final ScrollController scrollController;
   final String id;
   final String name;
 
-  const FeedScreen({super.key, required this.scrollController, required this.id, required this.name});
+  const FeedScreen({
+    super.key,
+    required this.scrollController,
+    required this.id,
+    required this.name,
+  });
 
   @override
   State<FeedScreen> createState() => _FeedScreenState();
 }
 
-class _FeedScreenState extends State<FeedScreen> {
+class _FeedScreenState extends State<FeedScreen>
+    with AutomaticKeepAliveClientMixin<FeedScreen> {
   final TweetFeedController _feedController = TweetFeedController();
   FeedTab? _tab;
+  bool _followingMediaOnly = false;
+
+  @override
+  bool get wantKeepAlive => true;
+
+  Future<void> _selectTab(BasePrefService prefs, FeedTab value) async {
+    if (_tab == value) return;
+
+    setState(() => _tab = value);
+    await prefs.set(optionHomeDefaultFeedTab, value.name);
+  }
 
   @override
   Widget build(BuildContext context) {
+    super.build(context);
+
     final BasePrefService prefs = PrefService.of(context);
     final available = availableFeedTabs(prefs);
-    var tab = _tab ??= feedTabFromId(prefs.get<String>(optionHomeDefaultFeedTab));
+    var tab =
+        _tab ??= feedTabFromId(prefs.get<String>(optionHomeDefaultFeedTab));
+
     // The plugin can be turned off while its feed is the one being shown.
     if (!available.any((e) => e.id == tab)) {
       tab = _tab = FeedTab.following;
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        prefs.set(optionHomeDefaultFeedTab, FeedTab.following.name);
+      });
     }
+
+    final sourceOptions = [
+      for (final option in available)
+        HomeSourceOption<FeedTab>(
+          value: option.id,
+          label: option.titleBuilder(context),
+          icon: feedTabIcon(option.id),
+        ),
+    ];
 
     return GroupFeedShell(
       scrollController: widget.scrollController,
       groupId: widget.id,
-      titleBuilder: (context) => DropdownMenu<FeedTab>(
-        initialSelection: tab,
-        inputDecorationTheme: const InputDecorationTheme(
-          border: InputBorder.none,
-          focusedBorder: InputBorder.none,
-          enabledBorder: InputBorder.none,
-        ),
-        dropdownMenuEntries:
-            available.map((e) => DropdownMenuEntry(value: e.id, label: e.titleBuilder(context))).toList(),
-        onSelected: (value) {
-          setState(() => _tab = value!);
-        },
+      titleBuilder: (context) => HomeSourceSwitcher<FeedTab>(
+        selected: tab,
+        options: sourceOptions,
+        onSelected: (value) => _selectTab(prefs, value),
       ),
       actionsBuilder: (context) {
         final model = context.read<GroupModel>();
@@ -86,12 +124,38 @@ class _FeedScreenState extends State<FeedScreen> {
           context,
           model: model,
           showMore: tab == FeedTab.following,
+          scrollToTopController: widget.scrollController,
         );
       },
-      bodyBuilder: (context) => switch (tab) {
-        FeedTab.following => SubscriptionGroupScreenContent(id: widget.id),
-        FeedTab.reddit => RedditFeedList(scrollController: widget.scrollController),
-        FeedTab.foryou => ForYouTweets(_feedController, type: 'profile', includeReplies: false, pref: prefs),
+      bodyBuilder: (context) {
+        final body = switch (tab) {
+          FeedTab.following => SubscriptionGroupScreenContent(
+              id: widget.id,
+              mediaOnly: _followingMediaOnly,
+            ),
+          FeedTab.reddit => RedditFeedList(
+              scrollController: widget.scrollController,
+            ),
+          FeedTab.foryou => ForYouTweets(
+              _feedController,
+              type: 'profile',
+              includeReplies: false,
+              pref: prefs,
+            ),
+        };
+
+        return Column(
+          children: [
+            if (tab == FeedTab.following)
+              HomeReadingControls(
+                mediaOnly: _followingMediaOnly,
+                onMediaToggle: () => setState(
+                  () => _followingMediaOnly = !_followingMediaOnly,
+                ),
+              ),
+            Expanded(child: body),
+          ],
+        );
       },
     );
   }
